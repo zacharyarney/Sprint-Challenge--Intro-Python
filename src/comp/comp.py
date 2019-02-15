43a243c410d22d7572a84f2666a9ad066a38f950
zacharyarney <zachary.arney@gmail.com>
2019-02-15 10:04:05 -0800

    MVP complete--changed an upper() to lower() in comp.py

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,7 +24,7 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = [h.name for h in humans if h.name[0].upper() == 'd'.upper()]
+a = [h.name for h in humans if h.name[0].lower() == 'd'.lower()]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
